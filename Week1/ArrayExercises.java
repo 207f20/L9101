63926ffc737e3bf4e13aa001921ee0d1ab4fc5de
207f20 <rain.sharmin@gmail.com>
2020-09-14 11:00:24 -0400

    bonus exercises

---
+++ Week1/ArrayExercises.java
@@ -0,0 +1,32 @@
+package sol;
+
+import java.util.ArrayList;
+
+public class ArrayExercises {
+
+	/**
+	 * Return the number of elements in the given array arr1
+	 * which also occur in the given ArrayList arr2.
+	 * 
+	 * @param arr1	an array of elements
+	 * @param arr2	an ArrayList of elements
+	 * 
+	 * @return	the number of common elements between an array and ArrayList
+	 */
+	public static int countSimilar(int[] arr1, ArrayList<Integer> arr2) {
+		
+		return 0;
+		
+	}
+
+	public static void main(String[] args) {
+
+		/**
+		 * Write code below that creates an array of elements
+		 * and an ArrayList of elements, and prints out the result
+		 * of calling the above function with these values.
+		 */
+		
+	}
+
+}
---
+++ Week1/HashmapExercise.java
@@ -0,0 +1,32 @@
+package sol;
+
+import java.util.HashMap;
+
+public class HashmapExercise {
+
+	/**
+	 * Return a HashMap that includes each unique element in the given
+	 * array as a key, and the number of times that element occurs in the
+	 * array as the value.
+	 * 
+	 * @param arr1	an array of elements
+	 * 
+	 * @return a hashmap that shows frequency count of elements in given array
+	 */
+	public static HashMap<Integer, Integer> countFrequencies(int[] arr1) {
+		
+		return null;
+		
+	}
+
+	public static void main(String[] args) {
+
+		/**
+		 * Write code below that creates an array of integers
+		 * and prints out the result of calling the above function
+		 * on it.
+		 */
+		
+	}
+	
+}
