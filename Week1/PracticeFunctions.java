fde87386b77e1c2f792a61a2faf31e9bbe522258
207f20 <rain.sharmin@gmail.com>
2020-09-14 10:57:19 -0400

    practice functions java file starter

---
+++ Week1/PracticeFunctions.java
@@ -0,0 +1,50 @@
+/** 
+ * Some practice functions for basic Java syntax.
+ * 
+ * @author sadiasharmin
+ *
+ */
+public class PracticeFunctions {
+
+	/**
+	 * Returns double the given number if the number is odd,
+	 * -1 if it's 0, or the same number back if it's even.
+	 * 
+	 * @param i	an integer input
+	 * @return -1 if the given int is 0, double the number if it's odd,
+	 * 			or return the same number back
+	 */
+	public static int get_num(int i) {
+		
+
+	}
+	
+	/**
+	 * Returns the sum of all even numbers up to the given integer.
+	 * 
+	 * @param n	any integer
+	 * @return the sum of all even integers up to n
+	 */
+	public static int add_evens(int n) {
+		
+
+	}
+	
+	/**
+	 * Returns the sum of all odd numbers up to the given integer.
+	 * 
+	 * @param n	any integer
+	 * @return the sum of all odd integers up to n
+	 */
+	public static int add_odds(int n) {
+		
+
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(get_num(5));
+		System.out.println(add_odds(8));
+
+	}
+}
