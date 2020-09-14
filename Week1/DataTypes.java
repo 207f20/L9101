eeb4c6c80d03329b9e22214ff7f72ee3375df62a
207f20 <rain.sharmin@gmail.com>
2020-09-14 10:12:49 -0400

    Data types exercise

---
+++ Week1/DataTypes.java
@@ -0,0 +1,106 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+        
+		/**
+		 * Q1: What's the difference between the variables below?
+		 * 
+		 * Your answer here:
+		 * 
+		 * 
+		 * 
+		 */
+		int num = 456;
+		Integer num2 = 456;
+		
+		/**
+		 * Q2.1: What's wrong with the following line? Explain. 
+		 * 
+		 * Your answer here:
+		 * 
+		 * 
+		 */
+
+		// Q2.2: Fix the line below.
+		i = 345;
+		
+		/** 
+		 * Q3.1: What's wrong with the following line? Explain.
+		 * 
+		 * Your answer here: 
+		 * 
+		 * 
+		 */
+
+		// Q3.2: Fix the line below.
+		float f = 3222.45;
+		
+		/**
+		 * Q4.
+		 * 
+		 * Write code that creates a new double variable (with any decimal 
+		 * number value that you want), and then prints it
+		 * converted to an integer.
+		 * 
+		 */
+		// Your code here:
+
+
+		/**
+		 * Q5.
+		 * 
+		 * Write code that creates a new integer variable (with any integer 
+		 * number value that you want), and then stores that value as a float
+		 * in a new float variable named f2.
+		 * 
+		 */
+		// Your code here: 
+		
+		
+		
+		
+		/**
+		 * Q6: What is happening in the lines of code below? Explain.
+		 * 
+		 * Your answer here:
+		 * 
+		 * 
+		 */
+		char c = 'A';
+		int c_as_int = c;
+		System.out.println(c_as_int);
+		
+
+		/**
+		 * Q7: Fix the code below (with minimal changes) so it prints A.
+		 */
+		int j = 65;
+		char c2 = j;
+		System.out.println(c2);
+		
+		
+		/**
+		 * Q8: What are all the type conversions happening below?
+		 * 
+		 * Your answer here:
+		 * 
+		 * 
+		 */
+		j = (int)((1.0/2) + ((double)1/2));
+		System.out.println("j = " + j);
+		
+		
+		/**
+		 * Q9: Write code to:
+		 * - Make two different strings stored in two different string variables.
+		 * - Make a third string variable whose value is the two above strings concatenated.
+		 * - Use any 2 string methods of your choice and print out the results of each.
+		 */
+		// Your code here
+		
+		
+		
+		
+	}
+}
